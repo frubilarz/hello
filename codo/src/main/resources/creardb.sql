585f0740248a2e128900f08e9d71324ae9aa331b
frubilarz <frubilar@icci.cl>
2013-12-08 22:49:28 -0300

    Update creardb.sql

--- codo/src/main/resources/creardb.sql
+++ codo/src/main/resources/creardb.sql
@@ -1,1 +1,4 @@
-select * from venta where idboleta =1;
+SET NAMES 'utf8';
+
+CREATE DATABASE cododb CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+GRANT ALL ON cododb.* TO codo@localhost IDENTIFIED BY 'codo123';
